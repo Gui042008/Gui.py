6a6dd8d21b47b45f9a5a73c64a717d326530ff88
Guilherme <guivjjig@gmail.com>
2025-06-30 12:03:51 -0300

    oooooo

--- telacliente.py
+++ telacliente.py
@@ -2,15 +2,39 @@ import getpass
 import csv
 from conta import Conta
 
-with open ('contas.csv',newline ='',encoding ='utf-8',erros ='ignore') as lerContas:
-    leitor =csv.reader(lerContas,delimiter =',')
-    for linha in leitor:
-        conta = conta(linha[0],linha[1],linha[3],linha[4])
-        conta.append(conta)
+#inicialização de variaveis
+contas = []
+acesso_liberado = False
 
+def lerArquivo():
+    with open('contas.csv', newline = '', encoding = 'utf-8', errors = 'ignore') as lerContas:
+        leitor = csv.reader(lerContas, delimiter = ',')
+        for linha in leitor:
+            conta = Conta(int(linha[0]), int(linha[1]),linha[2],float(linha[3]), linha[4])
+            contas.append(conta)    
 
-    agencia = int(input("Digite a agência: "))
-    numero_conta = int(input("Digite sua conta corrente: "))
-    senha = getpass.getpass("Digite a senha: ")
+lerArquivo()
+print(contas)
+
+agencia = int(input("Digite a agência: "))
+numero_conta = int(input("Digite sua conta corrente: "))
+senha = getpass.getpass("Digite a senha: ")
+
+# Encontrar a conta corrente na lista de contas
+contaEncontrada = None
+for conta in contas:
+    if numero_conta == conta.numero:
+        contaEncontrada = Conta(conta.agencia, conta.numero, conta.titular, conta.saldo, conta.senha)
+
+if contaEncontrada == None: # verifica se a conta é Nula (inexistente)
+    print("Dados incorretos")
+else:
+    # Verificar se o usuário digitou a senha e agência corretamente
+    if senha == contaEncontrada.senha and agencia == contaEncontrada.agencia:
+        print("Acesso liberado")
+    else:
+        print("Dados incorretos")
     
     
+
+	
